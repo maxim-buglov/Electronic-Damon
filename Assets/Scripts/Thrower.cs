c0ccb559f988d2f0c88dfbb95caf2b34eb7d1043
Maxim Buglov <maxim.buglov@gmail.com>
2018-06-17 11:37:27 +0300

    Thrower

---
+++ Assets/Scripts/Thrower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrower : MonoBehaviour {
+
+    public GameObject GameObject;
+    public int WaitSeconds;
+
+	// Use this for initialization
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return new WaitForSeconds(WaitSeconds);
+            Instantiate(GameObject, transform.position, transform.rotation);
+        } while (true);
+    }
+}
